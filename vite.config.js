d6c7bcd42269e95d78f9a6d63edaff14b83a0c70
skc4365 <50658047+skc4365@users.noreply.github.com>
2025-08-20 09:44:55 +0900

    update conf

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig(({ mode }) => {
   return {
     plugins: [react()],
-    base: mode === 'production' ? '/react-site3/' : '/',
+    base: mode === 'production' ? '/react-site/' : '/',
   }
 })
 // export default defineConfig({
